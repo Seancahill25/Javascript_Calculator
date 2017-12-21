96d4e7feefd33f9f17ca67fa64b64c7fa2fe54ca
Sean Cahill <seancahill25@gmail.com>
2017-12-21 00:58:48 +0000

    fixed javascript and css

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
+/* global $ */
 $(document).ready(function(){
-   //Stores the inputs from user to calculate later
   var inputs=[""];
-  //String to store current input string
   var totalString;
- //Operators array for validation without .
   var operators1 = ["+","-","/","*"];
- //operators array for validation with .
   var operators2 = ["."];
-  //Numbers array for validation
   var nums=[0,1,2,3,4,5,6,7,8,9];
-  //Adds value to inputs array and checks validation
+
   function getValue(input){
      if(operators2.includes(inputs[inputs.length-1])===true && input==="."){
     console.log("Duplicate '.'");
   }
-      //Validation so it doesn't start with an operator
     else if(inputs.length===1 && operators1.includes(input)===false){
       inputs.push(input);
     }
-    //If last character was not an operator add operator to the array
   else if(operators1.includes(inputs[inputs.length-1])===false){
     inputs.push(input);
   }
@@ -29,24 +23,24 @@ $(document).ready(function(){
   }
   function update(){
     totalString= inputs.join("");
-    $("#steps").html(totalString);
+    $("#display").html(totalString);
     console.log(inputs);
   }
   function getTotal(){
     totalString = inputs.join("");
-    $("#total").html(eval(totalString));
+    $("#display").html(eval(totalString));
   }
   
   $("a").on("click",function(){
-    if(this.id==="clearequation"){
-      inputs = [""];
+    if(this.id==="clearEquation"){
+      inputs = ["0"];
       update();
     }
     else if(this.id==="clear"){
       inputs.pop();
       update();
     }
-    else if(this.id==="="){
+    else if(this.id==="total"){
       getTotal();
     }
     else{
