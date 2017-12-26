513f7d998b55182ce102b3227d093d7db7e9a04c
Sean Cahill <seancahill25@gmail.com>
2017-12-26 20:06:36 +0000

    changed some css, added text overflow, and cleaned up html, and js

--- script.js
+++ script.js
@@ -1,57 +1,49 @@
 /* global $ */
-$(document).ready(function(){
-  var inputs=[""];
-  var totalString;
-  var operators1 = ["+","-","/","*"];
-  var operators2 = ["."];
-  var nums=[0,1,2,3,4,5,6,7,8,9];
+$(document).ready(function() {
+    var inputs = [""];
+    var totalString;
+    var operators1 = ["+", "-", "/", "*"];
+    var decimal = ["."];
+    var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  function getValue(input){
-     if(operators2.includes(inputs[inputs.length-1])===true && input==="."){
-    console.log("Duplicate '.'");
-  }
-    else if(inputs.length===1 && operators1.includes(input)===false){
-      inputs.push(input);
+    function getValue(input) {
+        if (decimal.includes(inputs[inputs.length - 1]) === true && input === ".") {
+            console.log("Duplicate '.'");
+        } else if (inputs.length === 1 && operators1.includes(input) === false) {
+            inputs.push(input);
+        } else if (operators1.includes(inputs[inputs.length - 1]) === false) {
+            inputs.push(input);
+        } else if (nums.includes(Number(input))) {
+            inputs.push(input);
+        }
+        update();
     }
-  else if(operators1.includes(inputs[inputs.length-1])===false){
-    inputs.push(input);
-  }
-    else if(nums.includes(Number(input))){
-      inputs.push(input);
+
+    function update() {
+        totalString = inputs.join("");
+        $("#display").html(totalString);
+        console.log(inputs);
     }
-    update();
-  }
-  function update(){
-    totalString= inputs.join("");
-    $("#display").html(totalString);
-    console.log(inputs);
-  }
-  function getTotal(){
-    totalString = inputs.join("");
-    $("#display").html(eval(totalString));
-  }
-  
-  $("a").on("click",function(){
-    if(this.id==="clearEquation"){
-      inputs = ["0"];
-      update();
+
+    function getTotal() {
+        totalString = inputs.join("");
+        $("#display").html(eval(totalString));
     }
-    else if(this.id==="clear"){
-      inputs.pop();
-      update();
-    }
-    else if(this.id==="total"){
-      getTotal();
-    }
-    else{
-      
-      if(inputs[inputs.length-1].indexOf("+","-","/","*",".")===-1){
-        getValue(this.id);
-      }
-      else {
-        getValue(this.id);
-      }
-    } 
-  });
-  
+    $("a").on("click", function() {
+        if (this.id === "clearEquation") {
+            inputs = ["0"];
+            update();
+        } else if (this.id === "clear") {
+            inputs.pop();
+            update();
+        } else if (this.id === "total") {
+            getTotal();
+        } else {
+            if (inputs[inputs.length - 1].indexOf("+", "-", "/", "*", ".") === -1) {
+                getValue(this.id);
+            } else {
+                getValue(this.id);
+            }
+        }
+    });
 });
